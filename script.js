a90de5bc178e65011ab6450cc8c00d38a5dc9f81
letamin <lethanhminh0901@gmail.com>
2020-08-01 14:47:39 +0700

    refactored

--- script.js
+++ script.js
@@ -2,39 +2,39 @@ const svg = d3.select('svg');
 const height = +svg.attr('height');
 const width = +svg.attr('width');
 
-const colorScale = d3.scaleOrdinal() //each element in the domain wil have the correspond value in the range (apple - '#c11d1d', lemon - yellow)
-    .domain(['apple', 'lemon'])
+//scaleOrdinal: each element in the domain will have the corresponding value from the range (typeA - '#c11d1d', typeB - yellow)
+const colorScale = d3.scaleOrdinal()
+    .domain(['typeA', 'typeB'])
     .range(['#c11d1d', 'yellow'])
 
-const radiusScale = d3.scaleOrdinal()
-    .domain(['apple', 'lemon'])
+const dimensionScale = d3.scaleOrdinal()
+    .domain(['typeA', 'typeB'])
     .range([50, 25])
 
-const render = (selection, { fruits }) => {
-    const circle = selection.selectAll('circle').data(fruits);
-    circle
-        .enter().append('circle')
-        .attr('cx', (d, i) => i * 100 + 200)
-        .attr('cy', height / 2)
-        .merge(circle)
-        .attr('r', d => radiusScale(d.type))
+const render = (selection, { cars }) => {
+    const car = selection.selectAll('rect').data(cars)
+    car.enter().append('rect')
+        .attr('x', (_d, i) => width / 6 + i * 80)
+        .attr('y', height / 2 - 100)
+        .merge(car)
+        .attr('height', d => dimensionScale(d.type))
+        .attr('width', d => dimensionScale(d.type))
         .attr('fill', d => colorScale(d.type))
 
-    circle.exit().remove()
+    car.exit().remove()
 }
 
-const makeFruit = (type) => ({ type })
-const fruits = d3.range(5).map(() => makeFruit('apple'))
+const makeCar = (type) => ({ type })
+const cars = d3.range(5).map(() => makeCar('typeA'))
 
-render(svg, { fruits }) // invoke render the first time with 5 elements
+render(svg, { cars }) // invoke render the first time with 5 elements
 
 setTimeout(() => {
-    fruits.pop();           // remove the last element from our array
-    render(svg, { fruits }) // invoke render second time with 4 elements
+    cars.pop();           // remove the last element from our array
+    render(svg, { cars }) // invoke render second time with 4 elements
 }, 1000);
 
-
 setTimeout(() => {
-    fruits[2].type = 'lemon'; // update the second element in the fruits array
-    render(svg, { fruits })   // invoke render to update the DOM elements
+    cars[2].type = 'typeB'; // update the element with index 2 in the cars array
+    render(svg, { cars })   // invoke render to update the DOM elements
 }, 2000);
